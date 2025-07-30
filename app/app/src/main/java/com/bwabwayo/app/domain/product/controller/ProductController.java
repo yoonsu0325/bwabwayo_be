c139c730d564c2a12ddf0e9bfb3d6b820454e9b2
BAE JUNSU <jsbae1023@gmail.com>
2025-07-31 01:09:36 +0900

    refactor: POST /api/products 리팩터링

--- app/app/src/main/java/com/bwabwayo/app/domain/product/controller/ProductController.java
+++ app/app/src/main/java/com/bwabwayo/app/domain/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.bwabwayo.app.domain.product.controller;
 
+import com.bwabwayo.app.domain.product.dto.ResponseMessage;
 import com.bwabwayo.app.domain.product.dto.request.ProductCreateRequestDTO;
 import com.bwabwayo.app.domain.product.dto.request.ProductSearchRequestDTO;
 import com.bwabwayo.app.domain.product.dto.response.MessageDTO;
@@ -26,13 +27,31 @@ public class ProductController {
     private final ProductService productService;
 
     @Operation(summary = "상품 등록")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "상품 등록 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductCreateResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "서버 오류로 상품 등록 실패",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = MessageDTO.class)
+                    )
+            )
+    })
     @PostMapping
     private ResponseEntity<?> createProduct(@ModelAttribute ProductCreateRequestDTO requestDTO){
         try{
             ProductCreateResponseDTO responseDTO = productService.createProduct(requestDTO);
             return ResponseEntity.ok(responseDTO);
         } catch(Exception e){
-            return ResponseEntity.status(500).body(new MessageDTO("상품 등록 중 서버에 오류가 발생하였습니다."));
+            return ResponseEntity.status(500).body(new MessageDTO(ResponseMessage.PRODUCT_CREATE_FAIL.getText()));
         }
     }
 
@@ -49,37 +68,25 @@ public class ProductController {
     })
     @GetMapping
     public ResponseEntity<?> getProducts(@ModelAttribute ProductSearchRequestDTO requestDTO) {
-        // 유효하지 않다면, 기본값으로 초기화
         if(requestDTO.getPage() < 1) requestDTO.setPage(1);
         if(requestDTO.getSize() < 0) requestDTO.setSize(100);
 
         try{
             ProductSearchResponseDTO response = productService.searchProducts(requestDTO);
-
             return ResponseEntity.ok(response);
         } catch(Exception e){
-            return ResponseEntity.status(500).body(new MessageDTO("상품 조회 중 서버에 오류가 발생하였습니다."));
+            return ResponseEntity.status(500).body(new MessageDTO(ResponseMessage.PRODUCT_SEARCH_FAIL.getText()));
         }
     }
 
     @Operation(summary = "상품 상세 조회")
     @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "상품 상세 정보 조회 성공",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = ProductDetailResponseDTO.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "500",
-                    description = "서버 오류 발생",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = MessageDTO.class)
-                    )
-            )
+            @ApiResponse(responseCode = "200", description = "상품 상세 정보 조회 성공",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductDetailResponseDTO.class))),
+            @ApiResponse(responseCode = "404", description = "상품이 존재하지 않음",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = MessageDTO.class))),
+            @ApiResponse(responseCode = "500", description = "서버 오류 발생",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = MessageDTO.class)))
     })
     @GetMapping("/{productId}")
     public ResponseEntity<?> getProductById(@PathVariable Long productId){
@@ -87,9 +94,9 @@ public class ProductController {
             ProductDetailResponseDTO productDetail = productService.getProductDetail(productId);
             return ResponseEntity.ok(productDetail);
         } catch (EntityNotFoundException e){
-            return ResponseEntity.status(404).body(new MessageDTO("상품을 찾을 수 없습니다."));
+            return ResponseEntity.status(404).body(new MessageDTO(ResponseMessage.PRODUCT_DETAIL_NOT_FOUND.getText()));
         } catch(Exception e){
-            return ResponseEntity.status(500).body(new MessageDTO("서버에서 오류가 발생하였습니다."));
+            return ResponseEntity.status(500).body(new MessageDTO(ResponseMessage.PRODUCT_DETAIL_FAIL.getText()));
         }
     }
 
@@ -102,14 +109,14 @@ public class ProductController {
     })
     @DeleteMapping("/{productId}")
     public ResponseEntity<MessageDTO> deleteProductById(@PathVariable Long productId){
-        // TODO: 사용자 권한 검사 필요
         try{
             productService.deleteProductById(productId);
+            return ResponseEntity.ok(new MessageDTO(ResponseMessage.PRODUCT_DELETE_SUCCESS.getText()));
         } catch (EntityNotFoundException e){
-            return ResponseEntity.status(404).body(new MessageDTO("삭제하려는 상품을 찾을 수 없습니다."));
+            return ResponseEntity.status(404).body(new MessageDTO(ResponseMessage.PRODUCT_DELETE_NOT_FOUND.getText()));
         } catch (Exception e){
-            return ResponseEntity.status(500).body(new MessageDTO("서버에서 오류가 발생하였습니다."));
+            return ResponseEntity.status(500).body(new MessageDTO(ResponseMessage.PRODUCT_DELETE_FAIL.getText()));
         }
-        return ResponseEntity.ok(new MessageDTO("상품을 삭제하였습니다."));
     }
 }
+
