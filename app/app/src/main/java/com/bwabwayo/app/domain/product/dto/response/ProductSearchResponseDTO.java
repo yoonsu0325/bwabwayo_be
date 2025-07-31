09637c05d13e2369eb64eaefdc70f43825ed2f67
배준수 <jsbae1023@gmail.com>
2025-07-31 15:31:49 +0900

    hotfix: POST /api/products/categories 수정

--- app/app/src/main/java/com/bwabwayo/app/domain/product/dto/response/ProductSearchResponseDTO.java
+++ app/app/src/main/java/com/bwabwayo/app/domain/product/dto/response/ProductSearchResponseDTO.java
@@ -1,13 +1,9 @@
 package com.bwabwayo.app.domain.product.dto.response;
 
-import com.bwabwayo.app.domain.product.domain.Product;
-import com.bwabwayo.app.domain.product.dto.ResponseMessage;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.domain.Page;
 
 
 import java.util.List;
@@ -28,33 +24,4 @@ public class ProductSearchResponseDTO {
     private Integer current; // 현재 페이지 번호
     private Integer totalPages; // 총 페이지 수
     private Long totalItems; // 총 항목 수
-
-    public static ProductSearchResponseDTO fromEntity(Page<Product> pageData) {
-        List<Product> content = pageData.getContent();
-
-        List<ProductSearchResultDTO> result = content.stream().map(p ->
-                ProductSearchResultDTO.builder()
-                        .product(ProductSimpleDTO.fromEntity(p))
-                        .seller(UserSimpleDTO.fromEntity(p.getSeller()))
-                        .build()
-        ).toList();
-
-        int current = pageData.getNumber() + 1;
-        int end = (int) Math.ceil(current / 10.0) * 10; // 마지막 페이지 블록
-        int start = end - 9; // 처음 페이지 블록
-        int last = Math.min(end, pageData.getTotalPages()); // 실제 마지막 페이지 블록
-
-
-        return ProductSearchResponseDTO.builder()
-                .message(ResponseMessage.PRODUCT_SEARCH_SUCCESS.getText())
-                .result(result)
-                .start(start)
-                .last(last)
-                .prev(current > 1)
-                .next(pageData.hasNext())
-                .current(current)
-                .totalPages(pageData.getTotalPages())
-                .totalItems(pageData.getTotalElements())
-                .build();
-    }
 }
