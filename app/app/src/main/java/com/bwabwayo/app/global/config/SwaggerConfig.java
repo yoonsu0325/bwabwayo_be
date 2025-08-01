3edbf37d73c31a322d40192d40ff145a3b717a44
LJW9354 <dlwodnjs9354@naver.com>
2025-08-02 07:04:27 +0900

    chore: swagger 설정변경

--- app/app/src/main/java/com/bwabwayo/app/global/config/SwaggerConfig.java
+++ app/app/src/main/java/com/bwabwayo/app/global/config/SwaggerConfig.java
@@ -6,32 +6,40 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI UMCstudyAPI() {
+    public OpenAPI openAPI(ServletContext servletContext) {
+        String serverName = servletContext.getVirtualServerName();  // 서버 도메인 정보
+        String baseUrl;
+
+        if (serverName != null && serverName.contains("ssafy.io")) {
+            baseUrl = "https://i13e202.p.ssafy.io/be"; // 배포 서버 주소
+        } else {
+            baseUrl = "http://localhost:8081"; // 로컬 개발 주소
+        }
+
         Info info = new Info()
                 .title("봐봐요 API 명세서")
                 .version("1.0.0");
 
         String jwtSchemeName = "JWT TOKEN";
-        // API 요청헤더에 인증정보 포함
+
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
-        // SecuritySchemes 등록
         Components components = new Components()
                 .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
                         .name(jwtSchemeName)
-                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .type(SecurityScheme.Type.HTTP)
                         .scheme("bearer")
                         .bearerFormat("JWT"));
 
         return new OpenAPI()
-                .addServersItem(new Server().url(""))
+                .addServersItem(new Server().url(baseUrl))
                 .info(info)
                 .addSecurityItem(securityRequirement)
                 .components(components);
