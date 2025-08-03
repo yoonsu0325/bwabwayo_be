e259e75ac797ea83febe87c0a76dfac4b0f0c61e
BAE JUNSU <jsbae1023@gmail.com>
2025-08-03 23:16:40 +0900

    docs: WishController Swagger 명세 추가

--- app/app/src/main/java/com/bwabwayo/app/domain/wish/controller/WishController.java
+++ app/app/src/main/java/com/bwabwayo/app/domain/wish/controller/WishController.java
@@ -9,9 +9,14 @@ import com.bwabwayo.app.domain.wish.dto.request.WishlistRequestDTO;
 import com.bwabwayo.app.domain.wish.dto.response.ExistsResponseDTO;
 import com.bwabwayo.app.domain.wish.dto.response.WishlistResponseDTO;
 import com.bwabwayo.app.domain.wish.service.WishService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -26,11 +31,18 @@ public class WishController {
     private final WishService wishService;
     private final ProductService productService;
 
-
+    
+    @Operation(summary = "내 위시리스트 조회")
+    @ApiResponse(
+            responseCode = "200",
+            description = "위시리스트 조회 성공",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = WishlistResponseDTO.class))
+    )
     @GetMapping
     public ResponseEntity<?> getAllMyWishes(
             @Valid @ModelAttribute WishlistRequestDTO requestDTO,
-            @Parameter(hidden = true) @LoginUser User user) {
+            @Parameter(hidden = true) @LoginUser User user
+    ) {
         int pageNo = requestDTO.getPageNo();
         int pageSize = requestDTO.getPageSize();
 
@@ -39,6 +51,8 @@ public class WishController {
         return ResponseEntity.ok(responseDTO);
     }
 
+    @Operation(summary = "내 위시리스트에 추가")
+    @ApiResponse(responseCode = "200", description = "위시리스트에 추가 성공")
     @PostMapping("/{productId}")
     public ResponseEntity<Void> addWishProduct(
             @PathVariable Long productId,
@@ -55,12 +69,15 @@ public class WishController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "내 위시리스트에서 삭제")
+    @ApiResponse(responseCode = "200", description = "위시리스트에서 삭제 성공")
     @DeleteMapping("/{productId}")
     public ResponseEntity<Void> removeWishProduct(
             @PathVariable Long productId,
             @Parameter(hidden = true) @LoginUser User user
     ) {
         Product product = productService.getProductById(productId);
+
         if(product == null){
             log.warn("찜 해제하려는 상품이 존재하지 않음: productId={}",productId);
             throw new NotFoundException("찜 해제하려는 상품이 존재하지 않음: productId="+productId);
@@ -71,11 +88,19 @@ public class WishController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "내 위시리스트에 등록 여부 조회")
+    @ApiResponse(
+            responseCode = "200"
+            , description = "내 위시리스트 등록 여부 조회 성공"
+            , content = @Content(mediaType = "application/json",  schema = @Schema(implementation = ExistsResponseDTO.class))
+    )
     @GetMapping("/{productId}")
     public ResponseEntity<?> isWishProduct(
             @PathVariable Long productId,
-            @Parameter(hidden = true) @LoginUser User user) {
+            @Parameter(hidden = true) @LoginUser User user
+    ) {
         Product product = productService.getProductById(productId);
+
         if(product == null){
             log.warn("찜 여부를 확인하려는 상품이 존재하지 않음: productId={}",productId);
             throw new NotFoundException("찜 여부를 확인하려는 상품이 존재하지 않음: productId="+productId);
