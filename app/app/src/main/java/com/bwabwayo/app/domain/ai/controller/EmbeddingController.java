ba87eb8887e523801c653dc7ff9f2b77a171bb6d
DESKTOP-DLOPF19\SSAFY <wkdwhd7598@naver.com>
2025-08-08 17:07:39 +0900

    feat: qdrant title, categorie 변수 추가

--- app/app/src/main/java/com/bwabwayo/app/domain/ai/controller/EmbeddingController.java
+++ app/app/src/main/java/com/bwabwayo/app/domain/ai/controller/EmbeddingController.java
@@ -28,12 +28,14 @@ public class EmbeddingController {
         // 1. title 추출
         Long id = ponintRequest.getId();
         String title = ponintRequest.getTitle();
+        String category = ponintRequest.getKategorie();
 
         // 2. 임베딩 벡터 추출
-        List<Double> vector = openAiClient.getEmbedding(title);
+        List<Double> titleVec = openAiClient.getEmbedding(title);
+        List<Double> categoryVec = openAiClient.getEmbedding(category);
 
         // 3. QdrantPointDto 생성
-        QdrantPointDto qdrantPointDto = QdrantPointDto.from(id, title, vector);
+        QdrantPointDto qdrantPointDto = QdrantPointDto.from(id, title, category, titleVec, categoryVec);
 
         embeddingService.saveToQdrant(qdrantPointDto);
         return ResponseEntity.ok("Qdrant에 벡터 저장 완료");
@@ -42,13 +44,17 @@ public class EmbeddingController {
     // 유사도 검색
     @PostMapping("/search")
     public ResponseEntity<List<SimilarResultResponse>> searchSimilarTitles(@RequestBody PonintRequest request) {
-        String inputTitle = request.getTitle();
+        String queryTitle = request.getTitle();
+        String queryCat   = request.getKategorie();
 
         // 벡터화
-        List<Double> queryVector = openAiClient.getEmbedding(inputTitle);
+        List<Double> qTitleVec = openAiClient.getEmbedding(queryTitle);
+        List<Double> qCatVec   = (queryCat != null && !queryCat.isBlank())
+                ? openAiClient.getEmbedding(queryCat)
+                : qTitleVec; // 카테고리 없으면 타이틀 벡터 재사용
 
         // 유사도 검색 (Top 3)
-        List<SimilarResultResponse> similarTitles = embeddingService.searchSimilarTitles(queryVector, 3);
+        List<SimilarResultResponse> similarTitles = embeddingService.searchSimilarTitles(qTitleVec, qCatVec, 3);
 
         return ResponseEntity.ok(similarTitles);
     }
