ffec15448aa5675cd5d9a03911dc2460b75a203b
BAE JUNSU <jsbae1023@gmail.com>
2025-08-10 00:12:21 +0900

    feat: 업로드 파일 포맷 검사

--- app/app/src/main/java/com/bwabwayo/app/global/storage/controller/StorageController.java
+++ app/app/src/main/java/com/bwabwayo/app/global/storage/controller/StorageController.java
@@ -1,18 +1,16 @@
 package com.bwabwayo.app.global.storage.controller;
 
-import com.bwabwayo.app.global.exception.BadRequestException;
+import com.bwabwayo.app.global.exception.dto.ErrorResponse;
 import com.bwabwayo.app.global.storage.dto.response.UploadFileResponse;
 import com.bwabwayo.app.global.storage.dto.response.UploadResponse;
-import com.bwabwayo.app.global.storage.service.S3Service;
+import com.bwabwayo.app.global.storage.exception.NotAllowedFileFormatException;
 import com.bwabwayo.app.global.storage.service.StorageService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,23 +24,18 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping(value = { "/api/storage"})
 public class StorageController {
+
     private final StorageService storageService;
-    private final S3Service s3Service;
 
     @Value("${storage.path.temp}")
     private String tempPath;
 
 
     @Operation(summary = "파일 업로드")
-    @ApiResponse(responseCode = "200",
-            description = "업로드 성공",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UploadResponse.class))
-    )
+    @ApiResponse(responseCode = "200", description = "업로드 성공")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<UploadResponse> uploadFiles(@RequestParam("files") List<MultipartFile> files, @RequestParam("dir") String dirName) {
         List<UploadFileResponse> result = new ArrayList<>();
-
-        validateImages(files);
 
         try{
             for (MultipartFile file : files) {
@@ -56,7 +49,7 @@ public class StorageController {
         } catch(Exception e){
             for (UploadFileResponse uploadFileDTO : result) {
                 try{
-                    s3Service.delete(uploadFileDTO.getKey());
+                    storageService.delete(uploadFileDTO.getKey());
                 } catch(Exception ex){
                     log.warn("이미지 삭제 실패 key={}", uploadFileDTO.getKey());
                 }
@@ -76,9 +69,7 @@ public class StorageController {
     @ApiResponse(responseCode = "200")
     @PostMapping(value = "/upload/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<UploadResponse> uploadImages(@RequestParam("files") List<MultipartFile> files) {
-        // 이미지 파일만 업로드 가능
-        validateImages(files);
-
+        ensureImages(files);
         return uploadFiles(files, tempPath);
     }
 
@@ -86,6 +77,7 @@ public class StorageController {
     @ApiResponse(responseCode = "200")
     @PostMapping(value = "/upload/product", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<UploadResponse> uploadProductImages(@RequestParam("files") List<MultipartFile> files) {
+        ensureImages(files);
         return uploadImages(files);
     }
 
@@ -93,6 +85,7 @@ public class StorageController {
     @ApiResponse(responseCode = "200")
     @PostMapping(value = "/upload/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<UploadResponse> uploadProfileImages(@RequestParam("files") List<MultipartFile> files) {
+        ensureImages(files);
         return uploadImages(files);
     }
 
@@ -100,6 +93,7 @@ public class StorageController {
     @ApiResponse(responseCode = "200")
     @PostMapping(value = "/upload/inquiry", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<UploadResponse> uploadInquiryImages(@RequestParam("files") List<MultipartFile> files) {
+        ensureImages(files);
         return uploadImages(files);
    }
 
@@ -117,29 +111,32 @@ public class StorageController {
     @Operation(summary = "파일 삭제")
     @ApiResponse(responseCode = "200", description = "삭제 성공")
     @DeleteMapping("/delete")
-    public ResponseEntity<Void> deleteFile(@RequestParam("key") String fileName) {
-        storageService.delete(fileName);
-        return ResponseEntity.noContent().build();
-    }
-
-    @Deprecated
-    @Operation(summary = "Presigned URL 생성")
-    @ApiResponse(responseCode = "200", description = "Presigned URL 생성 성공")
-    @GetMapping("/presigned-url")
-    public ResponseEntity<String> generatePresignedUrl(@RequestParam("key") String key, @RequestParam @Min(0) Long expiration) {
-        String presignedUrl = storageService.generatePresignedUrl(key, expiration * 1000L);
-        return ResponseEntity.ok(presignedUrl);
+    public ResponseEntity<Void> deleteFile(@RequestParam String key) {
+        storageService.delete(key);
+        return ResponseEntity.ok().build();
     }
 
 
-    private void validateImages(List<MultipartFile> files){
+    /* =========== 유틸리티 ======================== */
+
+    /**
+     * 이미지 파일인지 검증
+     */
+    private void ensureImages(List<MultipartFile> files){
         for (MultipartFile file : files) {
             String contentType = file.getContentType();
 
             if (contentType == null || !contentType.startsWith("image/")) {
-                log.warn("이미지 파일만 업로드할 수 있습니다: file={}, contentType={}", file.getOriginalFilename(), contentType);
-                throw new BadRequestException("이미지 파일만 업로드할 수 있습니다: file=" + file.getOriginalFilename() + " contentType=" + contentType);
+                throw new NotAllowedFileFormatException("이미지 파일만 업로드할 수 있습니다: file=" + file.getOriginalFilename() + " contentType=" + contentType);
             }
         }
+    }
+
+    /* =========== 예외 처리 ======================== */
+
+    @ExceptionHandler(NotAllowedFileFormatException.class)
+    public ResponseEntity<ErrorResponse> handleException(NotAllowedFileFormatException e) {
+        log.info(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of("400", e.getMessage()));
     }
 }
