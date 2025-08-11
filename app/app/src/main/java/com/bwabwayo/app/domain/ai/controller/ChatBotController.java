17a81155982dcb1997dc7b1ed0d1bd34032f2362
DESKTOP-DLOPF19\SSAFY <wkdwhd7598@naver.com>
2025-08-11 10:30:30 +0900

    feat: 추천한 상품 관련된 판매글 조회

--- app/app/src/main/java/com/bwabwayo/app/domain/ai/controller/ChatBotController.java
+++ app/app/src/main/java/com/bwabwayo/app/domain/ai/controller/ChatBotController.java
@@ -2,10 +2,16 @@ package com.bwabwayo.app.domain.ai.controller;
 
 import com.bwabwayo.app.domain.ai.dto.request.ChatBotRequest;
 import com.bwabwayo.app.domain.ai.dto.response.OpenAiRecommendationResponse;
+import com.bwabwayo.app.domain.ai.dto.response.RecommendAiProductResponse;
 import com.bwabwayo.app.domain.ai.service.ChatBotService;
+import com.bwabwayo.app.domain.product.domain.Product;
+import com.bwabwayo.app.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class ChatBotController {
 
     private final ChatBotService chatBotService;
+    private final ProductService productService;
 
     // 챗봇 응답 전체
     @PostMapping
@@ -21,5 +28,18 @@ public class ChatBotController {
         return ResponseEntity.ok(response);
     }
 
-    // 추천한 상품 유사도 검색
+    // 추천한 상품에 관련댄 판매글
+    @GetMapping
+    public ResponseEntity<List<RecommendAiProductResponse>> getRecommendation(@RequestParam String keyword){
+        List<Product> products = productService.recommendTopK(keyword,3);
+        List<RecommendAiProductResponse> reList = products.stream().map(Product->
+                RecommendAiProductResponse.builder()
+                        .id(Product.getId())
+                        .title(Product.getTitle())
+                        .category(Product.getCategory())
+                        .price(Product.getPrice())
+                        .productImages(Product.getProductImages())
+                        .build()).collect(Collectors.toList());
+        return ResponseEntity.ok(reList);
+    }
 }
